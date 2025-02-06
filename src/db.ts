82063a25ed220425ab85d9413dac7ea68703ce45
RadovanCipovic <131493117+RadovanCipovic@users.noreply.github.com>
2025-02-06 15:39:07 +0100

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,6 @@ import mysql from "mysql2/promise";
 export let db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "thuglife22",
+  password: "DBblog123",
   database: "blogapp",
 });
